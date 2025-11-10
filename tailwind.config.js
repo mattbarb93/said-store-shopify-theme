394c6d2436654fc50449e4e72bfdaee1d0c15aba
Matheus Barbosa <mattbarb93@gmail.com>
2025-11-10 17:14:47 -0500

    removing plug in configs under tailwind.config.js since its outdated

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,17 +13,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [
-    require("@tailwindcss/forms"),
-    plugin(function ({ addBase }) {
-      addBase({
-        '[type="search"]::-webkit-search-decoration': { display: "none" },
-        '[type="search"]::-webkit-search-cancel-button': { display: "none" },
-        '[type="search"]::-webkit-search-results-button': { display: "none" },
-        '[type="search"]::-webkit-search-results-decoration': {
-          display: "none",
-        },
-      });
-    }),
-  ],
+  plugins: [],
 };
